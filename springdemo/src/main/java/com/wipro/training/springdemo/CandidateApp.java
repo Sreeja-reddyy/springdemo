1c74f294f551a01b376dcb73fc95eb7d71c2add0
Sree gs <padidemsreejareddy4881@gmail.com>
2022-05-10 12:33:15 +0530

    spring demo commit

---
+++ springdemo/src/main/java/com/wipro/training/springdemo/CandidateApp.java
@@ -0,0 +1,15 @@
+package com.wipro.training.springdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CandidateApp {
+
+	public static void main(String[] args) {
+		ApplicationContext appCon=new ClassPathXmlApplicationContext("CandidateConfig.xml");
+        Candidate factory=(Candidate)appCon.getBean("cd1");
+        factory.disp();
+     
+        ((ClassPathXmlApplicationContext) appCon).close();
+	}
+	}
---
+++ springdemo/src/main/java/com/wipro/training/springdemo/Student.java
@@ -0,0 +1,21 @@
+package com.wipro.training.springdemo;
+
+public class Student {
+	
+	String name;
+	
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void display()
+    {
+        System.out.println("Hello "+name);
+    }
+	
+
+}
